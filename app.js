cac14ac76aafd224bcd98c4273c49fb585d6d49e
nava2105 <150807583+nava2105@users.noreply.github.com>
2024-12-03 13:46:28 -0500

    feat(📋 Change actions): Change the port to deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const os = require('os');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Obtener la dirección IP local
 const getLocalIP = () => {
